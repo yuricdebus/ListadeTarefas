88f63247287aaedfa9e6f6ccf2b6ab5e7b2696f8
Yuri Debus <yuri_debus@estudante.sc.senai.br>
2020-04-29 21:39:28 -0300

    Todos os arquivos do projeto

---
+++ app_lista_tarefas/tarefa.model.php
@@ -0,0 +1,21 @@
+<?php
+
+class tarefa {
+	private $id;
+	private $id_status;
+	private $tarefa;
+	private $data_cadastro;
+
+	public function __get($atributo) {
+		return $this->$atributo;
+	}
+
+	public function __set($atributo, $valor) {
+		$this->$atributo = $valor;
+		return $this;
+	}
+}
+
+?>
+
+
---
+++ app_lista_tarefas_public/tarefa_controller.php
@@ -0,0 +1,7 @@
+<?php
+
+	require_once '../../app_lista_tarefas/tarefa_controller.php';
+
+
+
+?>
